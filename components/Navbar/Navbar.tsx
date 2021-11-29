ce730d8044c19f3aaa43c13bec6bc4300623d5d4
rata sorin <ratasorin0@gmail.com>
2021-11-29 12:49:25 +0200

    Server Side Props was called in component, causing it not to fire.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,12 @@
-import type { NextPage, GetStaticPropsResult } from "next";
-import { InferGetStaticPropsType } from "next";
-// import getImages from "../../pages/api/hello";
-import { GetStaticProps } from "next";
+import type {
+  NextPage,
+  InferGetServerSidePropsType,
+  GetServerSidePropsResult,
+} from "next";
 import navbar from "./navbar.module.css";
 import NavbarElement from "./NavbarElement/NavbarElement";
-import fs from "fs";
-import path from "path";
 
-const Navbar: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  images,
-}) => {
+const Navbar: NextPage<{ images: HTMLImageElement[] }> = ({ images }) => {
   return (
     <div className={navbar.container}>
       {images.map((image, index) => (
@@ -18,24 +15,5 @@ const Navbar: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     </div>
   );
 };
-export async function getStaticProps(): Promise<
-  GetStaticPropsResult<{ images: HTMLImageElement[] }>
-> {
-  const Path = path.join(process.cwd(), "/public/NavbarImages");
-  const images: HTMLImageElement[] = [];
-  await fs.readdir(Path, (err, files: string[]) => {
-    files.map((file) => {
-      console.log(file);
-      const image = new Image();
-      image.src = file;
-      images.push(image);
-    });
-  });
-  console.log("Path");
-  return {
-    props: {
-      images: images,
-    },
-  };
-}
+
 export default Navbar;

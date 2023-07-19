6cd8f1184e73928d7db73f9e53f27989e2377c63
ratasorin <ratasorin0@gmail.com>
2023-07-20 00:32:14 +0300

    [Feature] Make navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,19 +8,19 @@ const Navbar: FC = () => {
   if (!open)
     return (
       <button
-        className="absolute top-10 left-10 p-2 rounded-full bg-zinc-800/75 hover:cursor-pointer z-30"
+        className="absolute left-5 top-5 sm:top-7 sm:left-7 lg:top-10 lg:left-10 p-2 rounded-full bg-zinc-800/75 hover:cursor-pointer z-30"
         onClick={() => setOpen(true)}
       >
-        <IoMdMenu className="text-5xl text-white" />
+        <IoMdMenu className="text-3xl sm:text-4xl lg:text-5xl text-white" />
       </button>
     );
   return (
     <div className="absolute top-0 left-0 w-screen h-screen z-30 flex flex-col lg:flex-row items-center justify-evenly p-10 bg-black/60">
       <button
-        className="absolute top-10 left-10 p-2 rounded-full bg-zinc-800/75 hover:cursor-pointer z-30"
+        className="absolute left-5 top-5 sm:top-7 sm:left-7 lg:top-10 lg:left-10 p-2 rounded-full bg-zinc-800/75 hover:cursor-pointer z-30"
         onClick={() => setOpen(false)}
       >
-        <IoMdClose className="text-5xl text-white" />
+        <IoMdClose className="text-3xl sm:text-4xl lg:text-5xl text-white" />
       </button>
 
       {SECTIONS.map((section) => (
@@ -32,8 +32,10 @@ const Navbar: FC = () => {
             alt={section}
             className="flex-1 w-full"
           />
-          <div className="bg-black/60 px-4 py-2 rounded-xl">
-            <h1 className="text-2xl font-mono text-white">{section}</h1>
+          <div className="bg-black/60 px-2 py-1 sm:px-3 sm:py-2 lg:px-4 lg:py-3 rounded-lg lg:rounded-xl">
+            <h1 className="text-base sm:text-lg lg:text-2xl font-mono text-white">
+              {section}
+            </h1>
           </div>
         </div>
       ))}

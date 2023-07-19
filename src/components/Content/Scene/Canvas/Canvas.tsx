e2a4e3b9778a851c69c44adea7d18023131a2d78
ratasorin <ratasorin0@gmail.com>
2023-07-19 23:06:05 +0300

    [Fix] Fix small cracks in the road

--- src/components/Content/Scene/Canvas/Canvas.tsx
+++ src/components/Content/Scene/Canvas/Canvas.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import React, { useRef, useEffect, useContext } from "react";
+import React, { useRef, useEffect } from "react";
 import { Render } from "../Scene";
 
 interface Props {
@@ -14,21 +14,6 @@ interface Canvas extends HTMLCanvasElement {
 
 const Canvas: NextPage<Props> = ({ width, height, render }) => {
   const canvasRef = useRef<Canvas>(null);
-  // const { setShouldBlur } = useContext(BlurContext);
-  /**
-   * **time** is the way we are going to keep track of when we should re-draw an image. (control the framerate)
-   * @member **elapsed** is the time between the **"start"** (that being the last repaint) and the current
-   * timestamp. We initialize this with the same value as **duration** so we can have the first paint instantaneous.
-   * @mem ber  **start** is the timestamp of the most recent re-draw. The default value is -1 as we don't know when
-   * the first paint will happen.
-   * @member  **duration** is the time that should pass between two consecutive paints to achieve a
-   * desired frame rate.
-   */
-  const time = {
-    elapsed: 1000 / 60,
-    start: 1000 / 60,
-    duration: 1000 / 60,
-  };
 
   useEffect(() => {
     if (width && height) {
@@ -40,19 +25,8 @@ const Canvas: NextPage<Props> = ({ width, height, render }) => {
        * We will need it in the cleanup function to stop the animation once the component is unmounted
        */
       let frameID: number;
-
-      /**
-       * The **render** function paints the browser at a certain framerate
-       * @param now **now** is the timestamp that requestAnimationFrame passes to the callback (that being
-       * the **render** function). In other words is the current timestamp
-       */
-      const loop = (now: number) => {
-        time.elapsed = now - time.start;
+      const loop = () => {
         render.update();
-        if (time.elapsed >= time.duration) {
-          time.start = now;
-          render.update();
-        }
         render.render();
         frameID = window.requestAnimationFrame(loop);
       };
@@ -61,7 +35,12 @@ const Canvas: NextPage<Props> = ({ width, height, render }) => {
     }
   }, [width, height]);
 
-  return <canvas ref={canvasRef} className="relative w-full h-full bg-transparent"></canvas>;
+  return (
+    <canvas
+      ref={canvasRef}
+      className="relative w-full h-full bg-transparent"
+    ></canvas>
+  );
 };
 
 export default Canvas;

a6b7997bc7bdb77d0cfbaffcb14f99f3c88203da
ratasorin <ratasorin0@gmail.com>
2023-07-19 14:10:42 +0300

    [Feature} Convert the road into svg

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,6 @@
 import { useState, useRef, useEffect, FC } from "react";
 import content from "./content.module.css";
-import Street from "./Street/Street";
 import Scene from "./Scene/Scene";
-// import BlurProvider from "context/animationContext";
 
 const MainScene: FC = () => {
   const [dimensions, setDimensions] = useState({
@@ -30,11 +28,11 @@ const MainScene: FC = () => {
       className="relative overflow-hidden w-screen flex-1 flex flex-col-reverse"
     >
       <Scene width={dimensions.width} height={dimensions.height}></Scene>
-      <Street></Street>
       <div className={content.clouds_back}></div>
       <div className={content.mountains}></div>
       <div className={content.clouds_front}></div>
       <div className={content.trees}></div>
+      <div className={content.street}></div>
     </div>
   );
 };

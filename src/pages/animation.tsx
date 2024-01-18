1a40d8d645650bc06a8e031c63dcd148ba0e070e
ratasorin <ratasorin0@gmail.com>
2024-01-18 12:09:26 +0200

    [Feature] Encapsulate svg rotation logic for cleaner code

--- src/pages/animation.tsx
+++ src/pages/animation.tsx
@@ -1,89 +1,25 @@
-import Head from "next/head";
-import React, { useCallback, useEffect, useRef } from "react";
-import { motionBlur } from "lib/motion-blur";
+import React, { useEffect } from "react";
+import EmbedSvg, { useSvg } from "lib/embed-svg";
+import { rotateElementAroundAnchorPoint } from "lib/rotate-svg";
 
-const rotateElementAroundAnchorPoint = (
-  elementId: string,
-  keyframes: [number, number, number],
-  enableMotionBlur: boolean = false
-) => {
-  const element = document.getElementById(elementId);
-  const elementAnchorPoint = document.getElementById(
-    `${elementId}_anchor_point`
-  );
-  const elementAnchorPointX = elementAnchorPoint?.getAttribute("cx");
-  const elementAnchorPointY = elementAnchorPoint?.getAttribute("cy");
-
-  if (!elementAnchorPointX || !elementAnchorPointY) return;
-
-  const elementAnimation = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "animateTransform"
-  );
-
-  elementAnimation.setAttribute("id", elementId);
-  elementAnimation.setAttributeNS(null, "attributeName", "transform");
-  elementAnimation.setAttributeNS(null, "attributeType", "XML");
-  elementAnimation.setAttributeNS(null, "type", "rotate");
-  elementAnimation.setAttributeNS(null, "calcMode", "spline");
-  elementAnimation.setAttributeNS(
-    null,
-    "values",
-    // DO NOT ADD SPACES BETWEEN SEMICOLON AND NUMBER! SAFARI ISSUE: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/keyTimes
-    `${keyframes[0]} ${elementAnchorPointX} ${elementAnchorPointY};${keyframes[1]} ${elementAnchorPointX} ${elementAnchorPointY};${keyframes[2]} ${elementAnchorPointX} ${elementAnchorPointY}`
-  );
-  elementAnimation.setAttributeNS(null, "keyTimes", "0; 0.5; 1");
-  elementAnimation.setAttributeNS(
-    null,
-    "keySplines",
-    "0.5 0 0.5 1; 0.5 0 0.5 1"
-  );
-  elementAnimation.setAttributeNS(null, "dur", "1.5s");
-  elementAnimation.setAttributeNS(null, "repeatCount", "indefinite");
-
-  element?.append(elementAnimation);
-
-  if (enableMotionBlur) motionBlur(elementId);
-};
+const EMPLOYEE_SVG_ID = "employee-svg";
 
 const Animation = () => {
-  const svgCreated = useRef(0);
-  const getSVG = useCallback(async () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "sistemgas-employee.svg", false);
-    // Following line is just to be on the safe side;
-    // not needed if your server delivers SVG with correct MIME type
-    xhr.overrideMimeType("image/svg+xml");
-    const svg: Promise<HTMLElement | null> = new Promise((resolve) => {
-      xhr.onload = () => {
-        if (xhr.responseXML) resolve(xhr.responseXML.documentElement);
-        else resolve(null);
-      };
-    });
+  const svg = useSvg(EMPLOYEE_SVG_ID);
 
-    xhr.send("");
-
-    return svg;
-  }, []);
   useEffect(() => {
-    if (document && !svgCreated.current) {
-      svgCreated.current = 1;
-      getSVG().then((svg) => {
-        if (!svg) return null;
-        document.getElementById("employee-container")?.append(svg);
-
-        rotateElementAroundAnchorPoint("hand", [-15, 15, -15], false);
-        rotateElementAroundAnchorPoint("forearm", [0, 18, 0], false);
-        rotateElementAroundAnchorPoint("right_arm", [0, 12, 0], true);
-
-        return null;
-      });
+    if (svg) {
+      rotateElementAroundAnchorPoint("hand", [-15, 15, -15], false);
+      rotateElementAroundAnchorPoint("forearm", [0, 18, 0], false);
+      rotateElementAroundAnchorPoint("right_arm", [0, 12, 0], true);
     }
-  }, []);
+  }, [svg]);
+
   return (
-    <>
-      <div id="employee-container"></div>;
-    </>
+    <EmbedSvg
+      elementId={EMPLOYEE_SVG_ID}
+      svgName="sistemgas-employee.svg"
+    ></EmbedSvg>
   );
 };
 

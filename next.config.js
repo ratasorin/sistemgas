c82c9b0b487d42b68c21f44ed6b1b236db059517
ratasorin <ratasorin0@gmail.com>
2024-01-21 02:19:29 +0200

    [Fix] Fix all `config` file issues (and force typescript to emit all typescript errors)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
-};
+module.exports = {};
